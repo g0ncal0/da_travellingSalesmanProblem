bef6c9bafb2881b02e9292cbbb20d11bffab8f5b
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2024-04-24 20:04:37 +0100

    first version of graph optimized for space and time

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#include "Graph/Graph.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Graph g = Graph(4);
+
+    g.setEdgeDistance(0, 1, 10.0, 1);
+    g.setEdgeDistance(0, 2, 50, 0);
+    g.setEdgeDistance(1, 2, 30, 1);
+    g.setEdgeDistance(3, 1, 40, 0);
+    g.setEdgeDistance(3, 2, 24, 1);
+    g.printDebug();
+
     return 0;
 }
